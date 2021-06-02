364151103c2b55a15de70d314b04e62781c083fe
Alexei Kireev <durobotdev@gmail.com>
2021-06-02 15:35:53 +0300

    Added a default value a help string to the 'message' positional CL  argument

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 struct Options
 {
+    // The following line is optional. It gives a default value
+    // to the 'message' positional argument.
+    // The rustdoc comment below it serves as a help string
+    // for StructOpt.
+    #[structopt(default_value = "Meow!")]
+    /// What does the cat say?
     message: String
 }
 
