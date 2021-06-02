3fbd1e1330353ac01bfdff6a14f378565801e530
Alexei Kireev <durobotdev@gmail.com>
2021-06-02 17:18:19 +0300

    Added (1) 'dead' flag; (2) check for 'woof' message / reaction to stderr

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,30 @@ struct Options
     // for StructOpt.
     #[structopt(default_value = "Meow!")]
     /// What does the cat say?
-    message: String
+    message: String,
+
+    // Add both short ('-d') and long versions ('--dead') of
+    // the following flag (named boolean argument, the value of which
+    // is determined by whether it is present or absent).
+    #[structopt(short = "d", long = "dead")]
+    /// Make the cat appear dead
+    dead: bool
 }
 
 fn main()
 {
     let options = Options::from_args();
     let message = options.message;
+    if message.to_lowercase() == "woof"
+    { eprintln!("A cat should not bark like a dog!"); } // print to stderr
+
     println!("{}", message);
     println!(" \\");
     println!("  \\");
     println!("   /\\_/\\");
-    println!("  ( o o )");
+    if options.dead
+    { println!("  ( x x )"); }
+    else
+    { println!("  ( o o )"); }
     println!("  =( I )=");
 }
