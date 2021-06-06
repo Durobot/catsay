b53576464debb920dbae0b20c3fb31ff0debde14
Alexei Kireev <durobotdev@gmail.com>
2021-06-06 08:18:16 +0300

    (1) Added fn fail_on_non_existing_file() test function; (2) Added simple output check to the test function fn run_with_defaults(), and predicates crate to Cargo.toml to make this check possible. Note that since catsay binary doesn't use this crate, the binary has not grown in size

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -5,6 +5,12 @@ use std::process::Command; // Run programs
 //    Finding your crate’s binary to test
 //    Assert on the result of your program’s run.
 use assert_cmd::prelude::*; // Add methods on commands
+// Crate predicates
+// This library implements an interface to "predicates" - boolean-valued functions of one argument.
+// This allows combinatorial logic to be created and assembled at runtime and then used one or more times for evaluating values.
+// Module predicates::prelude
+// Module that contains the essentials for working with predicates.
+use predicates::prelude::*; // For predicate::str::contains("Meow!")
 
 #[test]
 fn run_with_defaults() -> Result<(), Box<dyn std::error::Error>>
@@ -12,6 +18,22 @@ fn run_with_defaults() -> Result<(), Box<dyn std::error::Error>>
     Command::cargo_bin("catsay") // Create a Command to run a specific binary of the current crate
         .expect("binary doesn't exist") // as cargo_bin() returns Result<Self, CargoError>, we need to unwrap to get that Command
         .assert() // pub fn assert(&mut self) -> Assert : run a Command and make assertions on the struct std::process::Output
-        .success(); // Struct assert_cmd::assert::Assert : pub fn success(self) -> Self : Ensure the command succeeded
+        .success() // Struct assert_cmd::assert::Assert : pub fn success(self) -> Self : Ensure the command succeeded
+        .stdout(predicate::str::contains("Meow!")); // Function predicates::prelude::predicate::str::contains
+        // pub fn contains<P>(pattern: P) -> ContainsPredicate
+        //     where P: Into<String>,
+        // Creates a new Predicate that ensures a str contains pattern
+        // pub struct predicates::str::ContainsPredicate - Predicate that checks for patterns.
+    Ok(())
+}
+
+#[test]
+fn fail_on_non_existing_file() -> Result<(), Box<dyn std::error::Error>>
+{
+    Command::cargo_bin("catsay")
+        .expect("binary doesn't exist")
+        .args(&["-f", "no/such/file.txt"])
+        .assert()
+        .failure(); // Struct assert_cmd::assert::Assert : pub fn failure(self) -> Self : Ensure the command failed
     Ok(())
 }
